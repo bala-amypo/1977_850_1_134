9f96b11c938dcd722c97f1b1acc37c7fac1c72fd
bala-amypo <bala@amypo.in>
2025-12-19 13:06:53 +0000

    User commit

--- src/main/java/com/example/demo/service/ClinicalAlertService.java
+++ src/main/java/com/example/demo/service/ClinicalAlertService.java
@@ -5,13 +5,5 @@ import java.util.List;
 
 public interface ClinicalAlertService {
 
-    ClinicalAlertRecord createAlert(ClinicalAlertRecord alert);
-
-    ClinicalAlertRecord getById(Long id);
-
-    List<ClinicalAlertRecord> getAll();
-
     List<ClinicalAlertRecord> getAlertsByPatient(Long patientId);
-
-    void delete(Long id);
 }
--- src/main/java/com/example/demo/service/RecoveryCurveService.java
+++ src/main/java/com/example/demo/service/RecoveryCurveService.java
@@ -11,4 +11,3 @@ public interface RecoveryCurveService {
 
     List<RecoveryCurveProfile> getAll();
 }
-
