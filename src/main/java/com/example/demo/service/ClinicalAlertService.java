8d1a5652c8ae623d374689ac0103479360b0c050
bala-amypo <bala@amypo.in>
2025-12-19 13:42:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ClinicalAlertService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.model.ClinicalAlertRecord;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClinicalAlertService {
+
+    ClinicalAlertRecord createAlert(ClinicalAlertRecord alert);
+
+    List<ClinicalAlertRecord> getAlertsByPatient(Long patientId);
+
+    Optional<ClinicalAlertRecord> getAlertById(Long id);
+
+    ClinicalAlertRecord resolveAlert(Long alertId);
+
+    List<ClinicalAlertRecord> getAllAlerts();
+}
