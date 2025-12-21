c88dbfab2aa0a991644a2852669c3dc9a7daee97
bala-amypo <bala@amypo.in>
2025-12-21 08:39:00 +0000

    User commit

--- src/main/java/com/example/demo/service/PatientProfileService.java
+++ src/main/java/com/example/demo/service/PatientProfileService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PatientProfileService {
-
+ 
     PatientProfile createPatient(PatientProfile patient);
 
     Optional<PatientProfile> getPatientById(Long id);
