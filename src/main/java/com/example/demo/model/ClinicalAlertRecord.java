4e085505e7dfea42313d9b35890cf23b25f519e1
bala-amypo <bala@amypo.in>
2025-12-23 04:04:26 +0000

    User commit

--- src/main/java/com/example/demo/model/ClinicalAlertRecord.java
+++ src/main/java/com/example/demo/model/ClinicalAlertRecord.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClinicalAlertRecord {    
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
