ed2a1d88eae1d220a98c55dc30fe28ae04e2e8c4
bala-amypo <bala@amypo.in>
2025-12-25 09:37:12 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviationRule.java
+++ src/main/java/com/example/demo/model/DeviationRule.java
@@ -16,20 +16,20 @@ public class DeviationRule {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // REQUIRED BY TESTS
     @Column(nullable = false, unique = true)
     private String ruleCode;
 
     @Column(nullable = false)
     private String surgeryType;
 
-    // REQUIRED BY TESTS
     @Column(nullable = false)
     private String parameter;
 
-    // REQUIRED BY TESTS
     @Column(nullable = false)
     private Integer threshold;
+
+    @Column(nullable = false)
+    private String severity;
 
     @Column(nullable = false)
     private Boolean active;
