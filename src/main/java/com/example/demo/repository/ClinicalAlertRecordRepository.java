7d1751d7f805c30b677f24f8edd92c08ca66ebf0
bala-amypo <bala@amypo.in>
2025-12-25 11:11:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClinicalAlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/ClinicalAlertRecordRepository.java
@@ -1,17 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.ClinicalAlertRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface ClinicalAlertRecordRepository {
-
-    Optional<ClinicalAlertRecord> findById(Long id);
+@Repository
+public interface ClinicalAlertRecordRepository extends JpaRepository<ClinicalAlertRecord, Long> {
 
     List<ClinicalAlertRecord> findByPatientId(Long patientId);
-
-    List<ClinicalAlertRecord> findAll();
-
-    ClinicalAlertRecord save(ClinicalAlertRecord alert);
 }
