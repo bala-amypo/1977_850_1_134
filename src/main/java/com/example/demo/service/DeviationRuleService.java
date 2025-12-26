94c50a251371e1ad53996963137a49b860be357a
bala-amypo <bala@amypo.in>
2025-12-26 06:22:07 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviationRuleService.java
+++ src/main/java/com/example/demo/service/DeviationRuleService.java
@@ -6,14 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DeviationRuleService {
-
     DeviationRule createRule(DeviationRule rule);
-
-    Optional<DeviationRule> getRuleByCode(String ruleCode);
-
-    List<DeviationRule> getActiveRules();
-
-    List<DeviationRule> getAllRules();
-
     DeviationRule updateRule(Long id, DeviationRule rule);
+    List<DeviationRule> getAllRules();
+    List<DeviationRule> getActiveRules();
+    Optional<DeviationRule> getRuleByCode(String ruleCode);
 }
