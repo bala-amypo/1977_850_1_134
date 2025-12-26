7757aa5d80d655b96c329bb68cbe266b57408d70
bala-amypo <bala@amypo.in>
2025-12-26 03:14:28 +0000

    User commit

--- src/main/java/com/example/demo/model/DailySymptomLog.java
+++ src/main/java/com/example/demo/model/DailySymptomLog.java
@@ -8,17 +8,19 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "daily_symptom_logs")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class DailySymptomLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Long patientId;
 
+    @Column(nullable = false)
     private LocalDate logDate;
 
     private Integer painLevel;
